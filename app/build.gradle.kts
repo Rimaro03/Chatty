69f0ed1482fefea47251575e51ac0e7ccb9532be
leonardo <leonardoongaro4@gmail.com>
2025-05-03 19:48:44 +0200

    succesfully pushed input layout up on soft keyboard open

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.generativeai)
+    implementation(libs.androidx.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
